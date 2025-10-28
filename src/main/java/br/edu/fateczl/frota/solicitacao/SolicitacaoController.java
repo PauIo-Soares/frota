663a78cfa6afc5f8136f7fce4dbbf499e8cd3346
Paulo <paulohr625@gmail.com>
2025-10-27 23:37:57 -0300

    Front End Basico

--- src/main/java/br/edu/fateczl/frota/solicitacao/SolicitacaoController.java
+++ src/main/java/br/edu/fateczl/frota/solicitacao/SolicitacaoController.java
@@ -1,20 +1,35 @@
 package br.edu.fateczl.frota.solicitacao;
 
 import br.edu.fateczl.frota.orcamento.OrcamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 @RequestMapping("/solicitacao")
 public class SolicitacaoController {
 
-    private OrcamentoService orcamentoService = new OrcamentoService();
+    @Autowired
+    private OrcamentoService orcamentoService;
 
-    public void receberSolicitacao(SolicitacaoRequest request){
+    @GetMapping
+    public String mostrarFormulario() {
+        return "solicitacao";
+    }
 
-        System.out.println(orcamentoService.calcularFrete(request));
+    @PostMapping("/enviar")
+    public String receberSolicitacao(@RequestParam Double peso, @RequestParam Double comprimento, @RequestParam Double largura, @RequestParam Double altura, @RequestParam String cepOrigem, @RequestParam String cepDestino, Model model) {
+        SolicitacaoRequest request = new SolicitacaoRequest(peso, comprimento, largura, altura, cepOrigem, cepDestino);
+        Double valorFrete = orcamentoService.calcularFrete(request);
 
+        model.addAttribute("valorFrete", valorFrete);
+
+        return "resultado";
     }
 
 }
