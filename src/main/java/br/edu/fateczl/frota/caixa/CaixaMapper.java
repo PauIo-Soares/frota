38b5bcee3ea816f9a77212e46b727cd153a6a098
Paulo <paulohr625@gmail.com>
2025-10-30 15:40:15 -0300

    Mapper de Caixa

--- src/main/java/br/edu/fateczl/frota/caixa/CaixaMapper.java
+++ src/main/java/br/edu/fateczl/frota/caixa/CaixaMapper.java
@@ -1,8 +1,18 @@
 package br.edu.fateczl.frota.caixa;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface CaixaMapper {
+
+    @Mapping(target = "id", ignore = true)
+    Caixa toEntity(CaixaDTO dto);
+
+    CaixaDTO toDto(Caixa entity);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDto(CaixaDTO dto, @MappingTarget Caixa entity);
 
 }
--- src/main/java/br/edu/fateczl/frota/caixa/CaixaService.java
+++ src/main/java/br/edu/fateczl/frota/caixa/CaixaService.java
@@ -13,19 +13,13 @@ public class CaixaService {
     @Autowired
     private CaixaRepository caixaRepository;
 
-
-//  Talvez seja bom usar
-//  @Autowired
-//  private CaixaMapper caixaMapper;
+    @Autowired
+    private CaixaMapper caixaMapper;
 
     @Transactional
     public String criarCaixa(CaixaDTO dto) {
 
-        Caixa entidade = new Caixa();
-        entidade.setAltura(dto.altura());
-        entidade.setLargura(dto.largura());
-        entidade.setComprimento(dto.comprimento());
-        entidade.setLimitePeso(dto.limitePeso());
+        Caixa entidade = caixaMapper.toEntity(dto);
         caixaRepository.save(entidade);
 
         return "Caixa criada com sucesso!";
@@ -36,7 +30,7 @@ public class CaixaService {
 
         Caixa caixa = caixaRepository.findById(id).orElseThrow(() -> new RuntimeException("Caixa não encontrada"));
 
-        return new CaixaDTO(caixa.getId(), caixa.getAltura(), caixa.getLargura(), caixa.getComprimento(), caixa.getLimitePeso());
+        return caixaMapper.toDto(caixa);
 
     }
 
@@ -44,11 +38,7 @@ public class CaixaService {
     public String atualizarCaixa(CaixaDTO dto) {
 
         Caixa caixa = caixaRepository.findById(dto.id()).orElseThrow(() -> new RuntimeException("Caixa não encontrada"));
-
-        if (dto.altura() != null) caixa.setAltura(dto.altura());
-        if (dto.largura() != null) caixa.setLargura(dto.largura());
-        if (dto.comprimento() != null) caixa.setComprimento(dto.comprimento());
-        if (dto.limitePeso() != null) caixa.setLimitePeso(dto.limitePeso());
+        caixaMapper.updateEntityFromDto(dto, caixa);
         caixaRepository.save(caixa);
 
         return "Caixa atualizada com sucesso!";
@@ -59,21 +49,13 @@ public class CaixaService {
     public String excluirCaixa(Long id) {
 
         caixaRepository.deleteById(id);
+
         return "Caixa excluida com sucesso!";
 
     }
 
     public List<CaixaDTO> listarCaixas() {
-
-        List<Caixa> listaEntidades = caixaRepository.findAll();
-        List<CaixaDTO> resposta = new ArrayList<>();
-
-        for (Caixa i : listaEntidades) {
-            resposta.add(new CaixaDTO(i.getId(), i.getAltura(), i.getLargura(), i.getComprimento(), i.getLimitePeso()));
-        }
-
-        return resposta;
-
+        return caixaRepository.findAll().stream().map(caixaMapper::toDto).toList();
     }
 
 }
