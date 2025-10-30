b21e919f66dc8d03b748076121a0a3984f58c573
Paulo <paulohr625@gmail.com>
2025-10-29 21:02:55 -0300

    Refatoracao

--- src/main/java/br/edu/fateczl/frota/orcamento/OrcamentoService.java
+++ src/main/java/br/edu/fateczl/frota/orcamento/OrcamentoService.java
@@ -22,30 +22,26 @@ public class OrcamentoService {
     private static final Double PRECO_POR_KG = 0.50;
     private static final Double PRECO_MEDIO_PEDAGIO_CAMINHAO = 30.00;
     private static final Double PRECO_POR_CAIXA = 10.0;
+    private static final Double MEDIA_PEDAGIO_POR_KM = 944.44;
 
     public Double calcularFrete(SolicitacaoRequest solicitacao) {
-
-        Double valorFrete;
         Double volume = solicitacaoService.calcularVolume(solicitacao.altura(), solicitacao.comprimento(), solicitacao.largura());
+
         Double pesoCubado = solicitacaoService.calcularPesoCubado(volume);
+        Double pesoParaCobranca = Math.max(pesoCubado, solicitacao.peso());
+        Double valorCobrancaPorPeso = pesoParaCobranca * PRECO_POR_KG;
+        Double valorCobrancaPorCaixa = PRECO_POR_CAIXA;
 
-        Double tipoPesoCobranca = Math.max(pesoCubado, solicitacao.peso());
-        Double pesoCobranca = tipoPesoCobranca * PRECO_POR_KG;
-
-        Double caixaCobranca = 1 * PRECO_POR_CAIXA;
-
-        Double tipoCobranca = Math.max(pesoCobranca, caixaCobranca);
-
-        valorFrete = tipoCobranca;
+        Double valorFrete = Math.max(valorCobrancaPorPeso, valorCobrancaPorCaixa);
 
         Double distanciaKm = googleDistanciaService.calcularDistanciaEmKm(solicitacao.cepOrigem(), solicitacao.cepDestino());
 
         valorFrete += calcularPrecoDistancia(distanciaKm);
         valorFrete += calcularPrecoPedagios(distanciaKm);
 
-        valorFrete = BigDecimal.valueOf(valorFrete).setScale(2, RoundingMode.HALF_UP).doubleValue();
+        Double valorFreteArredondado = BigDecimal.valueOf(valorFrete).setScale(2, RoundingMode.HALF_UP).doubleValue();
 
-        return valorFrete;
+        return valorFreteArredondado;
 
     }
 
@@ -55,9 +51,11 @@ public class OrcamentoService {
 
     private Double calcularPrecoPedagios(Double distanciaKm) {
 
-        Double qtdPedagiosEmMedia = (distanciaKm / 50.61);
+        if (distanciaKm < MEDIA_PEDAGIO_POR_KM) return 0.0;
 
-        return qtdPedagiosEmMedia * PRECO_MEDIO_PEDAGIO_CAMINHAO;
+        Double qtdPedagios = distanciaKm / MEDIA_PEDAGIO_POR_KM;
+
+        return qtdPedagios * PRECO_MEDIO_PEDAGIO_CAMINHAO;
 
     }
 
