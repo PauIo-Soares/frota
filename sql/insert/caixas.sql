64ed58706b0cc8eb54e547a8bf5aa65a6922f840
Paulo <paulohr625@gmail.com>
2025-10-30 13:10:52 -0300

    Arrumando insert para popular BD para testes

---
+++ sql/insert/caixas.sql
@@ -0,0 +1,9 @@
+USE db_frota
+GO
+
+INSERT INTO tb_caixas (comprimento, largura, altura, limite_peso) VALUES
+(12.5, 15.0, 18.0, 16.0),
+(14.0, 12.0, 19.5, 17.0),
+(10.5, 11.5, 13.0, 12.0),
+(16.0, 18.0, 14.5, 19.0),
+(19.5, 17.0, 15.0, 18.5);
---
+++ sql/insert/marcas.sql
@@ -0,0 +1,8 @@
+USE db_frota
+GO
+
+INSERT INTO tb_marcas (nome,pais) VALUES
+('Mercedes', 'Brasil'),
+('Volvo', 'Argentina'),
+('Scania', 'Holanda'),
+('Volks', 'Alemanha');
